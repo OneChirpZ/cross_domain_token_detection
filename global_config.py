7c88dbf759b3562c402f12336721b4cab6a97233
accio <acciomerlincolin@gmail.com>
2024-04-01 16:41:41 +0800

    新增于cookies和post body里面寻找tokn

--- global_config.py
+++ global_config.py
@@ -7,6 +7,7 @@ token_others = ['jwt', 'bearer']
 stop_words = list({s.lower() for s in ['accept-encoding', 'connection', 'content-length', 'content-type',
                                        'host', 'hosts', 'accept-encoding', 'accept-language', 'user-agent',
                                        'sessionid', 'referer', 'cache-control', 'X-MMe-Client-Info']})
+# value_stop_words = list({s.lower() for s in [' ', '\%']}) # 进一步过滤掉不符合token条件的value
 
 # （compare）提取的 value 的最小长度
 min_token_len = 8
